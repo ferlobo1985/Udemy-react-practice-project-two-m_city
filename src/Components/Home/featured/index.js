d6c03fef5bda66677025cf5f8963bc0a1aa64314
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-04 18:25:19 -0300

    12-Finishing the animations

--- src/Components/Home/featured/index.js
+++ src/Components/Home/featured/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Test from './test';
 import Stripes from './Stripes';
+import Text from './Text';
 
 const Featured = () => {
     return(
         <div className="featured_wrapper">
             <Stripes/>
+            <Text/>
         </div> 
     )
 }
