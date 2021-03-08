6d62187aa0e79c50365881c816ecceb76ee167bd
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-03-08 01:35:43 -0300

    38-Not found

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import Home from './Components/Home';
 import SignIn from './Components/Signin';
 import TheTeam from './Components/theTeam';
 import TheMatches from './Components/theMatches';
+import NotFound from './Components/not_found';
 
 import Dashboard from './Components/Admin/Dashboard';
 import AdminPlayers from './Components/Admin/players';
@@ -23,21 +24,22 @@ const Routes = ({user}) => {
     <BrowserRouter>
       <Header user={user}/>
       <Switch>
-        <Route path="/admin_matches/edit_match/:matchid" exact component={AuthGuard(AddEditMatch)}/>
-        <Route path="/admin_matches/add_match" exact component={AuthGuard(AddEditMatch)}/>
-        <Route path="/admin_matches" exact component={AuthGuard(AdminMatches)}/>
+        <Route path="/admin_matches/edit_match/:matchid" component={AuthGuard(AddEditMatch)}/>
+        <Route path="/admin_matches/add_match" component={AuthGuard(AddEditMatch)}/>
+        <Route path="/admin_matches" component={AuthGuard(AdminMatches)}/>
 
-        <Route path="/admin_players/edit_player/:playerid" exact component={AuthGuard(AddEditPlayer)}/>
-        <Route path="/admin_players/add_player" exact component={AuthGuard(AddEditPlayer)}/>
-        <Route path="/admin_players" exact component={AuthGuard(AdminPlayers)}/>
+        <Route path="/admin_players/edit_player/:playerid" component={AuthGuard(AddEditPlayer)}/>
+        <Route path="/admin_players/add_player" component={AuthGuard(AddEditPlayer)}/>
+        <Route path="/admin_players" component={AuthGuard(AdminPlayers)}/>
 
-        <Route path="/dashboard"  component={AuthGuard(Dashboard)}/>
-        <Route path="/the_matches"  component={TheMatches}/>
-        <Route path="/the_team"  component={TheTeam}/>
-        <Route path="/sign_in" exact component={ 
+        <Route path="/dashboard" component={AuthGuard(Dashboard)}/>
+        <Route path="/the_matches" component={TheMatches}/>
+        <Route path="/the_team" component={TheTeam}/>
+        <Route path="/sign_in" component={ 
           props => (<SignIn {...props} user={user}/>) 
         }/>
         <Route path="/" exact component={Home}/>
+        <Route component={NotFound}/>
       </Switch>
       <ToastContainer />
       <Footer/>
